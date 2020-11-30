2cada73355cd44b102a320e10f1ab6c1504f7ec8
Felix Wolf <x3mdroid@gmail.com>
2020-11-30 19:31:16 +0100

    fixed a bug where a table could not be parsed

--- matchFinder/matchCalculator.py
+++ matchFinder/matchCalculator.py
@@ -1,21 +1,18 @@
 from munkres import Munkres, print_matrix
 import pandas as pd
 import numpy as np
-
-
+import random
 
 def calculateMatch(file):
     df = pd.read_csv(file)
-    df.replace([np.NAN, "Sechstwahl", "Fünftwahl", "Viertwahl", "Drittwahl", "Zweitwahl", "Erstwahl"],[np.infty, 6, 5, 4, 3, 2, 1], inplace=True)
-    df = df.iloc[:,1:]
-    matrix = np.array(df.values.tolist())
-    print(matrix)
-    m = Munkres()
-    matrix = [[1, 2, 3, 4],
-                [4, 3, 2, 1],
-                [1, 3, 2, 4],
-                [4, 2, 3, 1]]
-    indexes = m.compute(matrix)
+    newDf = df.replace([np.NAN, "Sechstwahl", "Fünftwahl", "Viertwahl", "Drittwahl", "Zweitwahl", "Erstwahl"],
+        [np.infty, 6, 5, 4, 3, 2, 1],)
+    #for item in df.values.tolist():
+    #   print(item)
+    newDf = newDf.iloc[:,1:]
+    matrix = np.array(newDf.values.tolist()).tolist()
+    random.shuffle(matrix)
+    indexes = Munkres().compute(matrix)
     print(indexes)
     print_matrix(matrix, msg='Lowest cost through this matrix:')
     total = 0
@@ -23,5 +20,5 @@ def calculateMatch(file):
         value = matrix[row][column]
         total += value
         print(f'({row}, {column}) -> {value}')
-    print(f'total cost: {total}')
-    
+        #print(df[row, column])
+    print(f'total cost: {total}, lowest possible cost: {len(matrix)}')
