d79d906a7037e5b14028c81618a1631bcbf5e0ac
Felix Wolf <x3mdroid@gmail.com>
2020-12-11 14:59:12 +0100

    flask_limiter warning behoben

--- matchFinder/__init__.py
+++ matchFinder/__init__.py
@@ -7,7 +7,7 @@ from flask_limiter.util import get_remote_address
 
 
 db = SQLAlchemy()
-limiter = Limiter()
+limiter = Limiter(key_func=get_remote_address)
 blocked_ip = []
 
 def create_app(test_config=None):
