20e5325d177f1890aad3552712558c511197b88d
Felix Wolf <x3mdroid@gmail.com>
2020-12-12 16:05:15 +0100

    Favicon hinzugefügt

--- matchFinder/upload.py
+++ matchFinder/upload.py
@@ -1,6 +1,5 @@
 from flask import (
     Blueprint, Flask, flash, g, redirect, render_template, request, session, url_for, abort, current_app as app)
-from . import db
 from . import database_helper
 
 bp = Blueprint('upload', __name__, url_prefix='/upload')
