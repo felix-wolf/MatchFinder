e92498a8e7a16c2e98f76db470421d6fe1980dde
Felix Wolf <x3mdroid@gmail.com>
2021-02-04 10:09:49 +0100

    Code dokumentiert für alle WTForm Klassen

--- matchFinder/forms/teilnehmer_form.py
+++ matchFinder/forms/teilnehmer_form.py
@@ -3,11 +3,22 @@ from wtforms import StringField, IntegerField, FieldList, FormField
 from wtforms.validators import DataRequired
 
 class TeilnehmerEntryForm(FlaskForm):
+	"""
+	defines a WTForm for the a single teilnehmer with 3 fields.
+	first_name and matr_nr are required while last_name is optional.
+	"""
 	first_name = StringField('Vorname', validators=[DataRequired()])
 	last_name = StringField('Nachname')
 	matr_nr = IntegerField('Matrikelnummer', validators=[DataRequired()])
 
 
 class TeilnehmerForm(FlaskForm):
+	"""
+	this class defines a WTForm for a list of teilnehmer.
+	It does so by asking for a name and a list of teilnehmerEntries,
+	which are defined above.
+	Both the name and the list are required.
+	"""
 	teilnehmer_name = StringField('Name', validators=[DataRequired()])
-	teilnehmer = FieldList(FormField(TeilnehmerEntryForm), validators=[DataRequired()])
+	teilnehmer = FieldList(FormField(TeilnehmerEntryForm),
+		validators=[DataRequired()])
